08a9774b99faedea0de559fc9a7f0390f429f546
lucasgmarcellino <lucasgabriel.marcellino@gmail.com>
2022-09-15 14:14:17 -0300

    Alteração comentario

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -4,8 +4,8 @@ import 'package:dotenv/dotenv.dart' show env, load;
 class ApplicationConfig {
   Future<void> loadConfigApplication() async {
     await _loadEnv();
-     final variavel = env['OneDrive'];
-     print(variavel);
+    //  final variavel = env['OneDrive'];
+    //  print(variavel);
  
   }
   Future<void> _loadEnv() async => load();
